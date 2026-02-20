ccafb9e9819afdee8e6e8ae4296c9c97dc4cb66b
Sai Santhosh S <saisanthosh10a07@gmail.com>
2026-02-20 14:36:35 +0530

    feat: add tools module init

---
+++ src/tools/__init__.py
@@ -0,0 +1,9 @@
+"""
+AuditAI - Tools Module
+
+MCP tools that the agent can invoke to gather information.
+- base.py: Abstract base tool class
+- price_feed.py: Token price data tool
+- news_feed.py: Crypto news aggregation tool
+- onchain_data.py: Weilchain on-chain data tool
+"""
